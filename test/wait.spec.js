16480a31e277021ad0a055d24ca35d798787ac29
y.shilkina <y.shilkina@a1qa.com>
2020-01-21 19:50:52 +0300

    add forFalse() method

--- test/wait.spec.js
+++ test/wait.spec.js
@@ -5,10 +5,18 @@ const Wait = require('../utils/wait.util')
 describe('Wait Test', () => {
     it('should wait for true "true"', () => {
         const wait = new Wait()
-        return wait.forTrue(() => true, 5, 1000, 0).then((result) => assert.isTrue(result))       
+        return wait.forTrue(() => true, 5, 1000).then((result) => assert.isTrue(result))       
     })
     it('should wait for true "false"', () => {
         const wait = new Wait()
-        return wait.forTrue(() => false, 5, 1000, 0).then((result) => assert.isFalse(result))       
+        return wait.forTrue(() => false, 5, 1000).then((result) => assert.isFalse(result))       
+    })
+    it('should wait for false "false"', () => {
+        const wait = new Wait()
+        return wait.forFalse(() => false, 5, 1000).then((result) => assert.isFalse(result))       
+    })
+    it('should wait for false "true"', () => {
+        const wait = new Wait()
+        return wait.forFalse(() => true, 5, 1000).then((result) => assert.isTrue(result))       
     })
 })
--- utils/wait.util.js
+++ utils/wait.util.js
@@ -1,27 +1,52 @@
 const logger = require('../utils/log.util');
 
-const doWait = (action, interval) => {
+const doWait = (action, interval, expectedValue) => {
     return new Promise((resolve, reject) => {
-        if(action()) {
+        if(action() === expectedValue) {
             setTimeout(() => resolve(), interval)
         }
         setTimeout(() => reject(), interval)
     })
 }
 
+var count = 0
+
+const retrier = () => {  
+}
+
+
 class Wait {
-    forTrue(action, maxCount, interval, count = 0) {
+    forTrue(action, maxCount, interval) {
         count++
         logger.info(`[${count}] Wait for true`)
-        return doWait(action, interval).then(() => {
+        return doWait(action, interval, true).then(() => {
             logger.warning('Was able to reach expected condition')
+            count = 0
             return true
         }, () => {
             if(maxCount <= count) {
-                logger.warning('Wasn\'t able to reach expected condition')
+                logger.warning(`Wasn\'t able to reach expected condition. The last received value is "${action()}"`)
+                count = 0
                 return false
             } else {
-                return this.forTrue(action, maxCount, interval, count)
+                return this.forTrue(action, maxCount, interval)
+            }
+        })      
+    }
+    forFalse(action, maxCount, interval) {
+        count++
+        logger.info(`[${count}] Wait for false`)
+        return doWait(action, interval, false).then(() => {
+            logger.warning('Was able to reach expected condition')
+            count = 0
+            return false
+        }, () => {
+            if(maxCount <= count) {
+                logger.warning(`Wasn\'t able to reach expected condition. The last received value is "${action()}"`)
+                count = 0
+                return true
+            } else {
+                return this.forFalse(action, maxCount, interval)
             }
         })      
     }
