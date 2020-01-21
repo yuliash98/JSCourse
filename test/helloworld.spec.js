c429bc3310aaff0baf1626cecdb5b43ebe1c9e6a
y.shilkina <y.shilkina@a1qa.com>
2020-01-21 15:49:34 +0300

    done ws1 homework

--- test/helloworld.spec.js
+++ test/helloworld.spec.js
@@ -1,16 +1,8 @@
 const {describe, it} = require('mocha');
-const {assert, expect} = require('chai');  
 const logger = require('../utils/log.util');
-const date = require('../utils/dateTime.util.js');
 
 describe('Hello World TestSuite', () => {
     it('should write "Hello World"', () => {
         logger.info('Hello World');
-    })
-})
-
-describe('DateTimeUtil TestSuite', () => {
-    it('should write today Date', () => {
-        logger.info(date.today())
     })
 })
